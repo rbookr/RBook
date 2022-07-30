310dcf4a933f80370de077bad99c1fd50d846588
rainboy <rainboylvx@qq.com>
2022-07-30 21:15:29 +0800

    mv file path

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,19 +1,20 @@
-import {markdownEjs}from 'rbookr-theme'
+import {markdownEjs} from 'rbookr-theme'
+import cppbookSiderBar from '../cppbook/siderbar'
 //import anchor from 'markdown-it-anchor'
 
 
 export default {
 
   lang: 'zh-CN',
-  title: 'CppBook',
+  title: 'RBook',
   description: 'CppBook -- online book of C++',
   appearance: true,
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
-    ['link', { rel: "manifest", href: "/site.webmanifest"}],
-    ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/rbookIcon/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/rbookIcon/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/rbookIcon/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/rbookIcon/site.webmanifest"}],
+    ['link', { rel: "shortcut icon", href: "/rbookIcon/favicon.ico"}],
   ],
 
   markdown: {
@@ -35,40 +36,27 @@ export default {
 
 
   themeConfig: {
-    logo:'/favicon.ico',
+    logo:'/rbookIcon/favicon.ico',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/rbookr/cpp_for_beginners' }
     ],
 
-    sidebar: [
-      {
-        text:'快速入门',
-        collapsible: true,
-        collapsed: false,
-        items:[
-          { text: '1.第一个程序', link: '/cpp_quick_guide/1-第一个程序.md' },
-          { text: '2.注释', link: '/cpp_quick_guide/2-注释.md' },
-          {text:'3.数据类型',link:'/cpp_quick_guide/3-数据类型.md' },
-          {text:'4.输入输出',link:'/cpp_quick_guide/4-输入输出.md' },
-          {text:'5.变量',link:'/cpp_quick_guide/5-变量.md'     },
-          {text:'6.运算符',link:'/cpp_quick_guide/6-运算符.md'   },
-          {text:'7.控制结构',link:'/cpp_quick_guide/7-控制结构.md' },
-          {text:'8.循环结构',link:'/cpp_quick_guide/8-循环结构.md' },
-          {text:'9.数组',link:'/cpp_quick_guide/9-数组.md'     },
-          {text:'10.字符串',link:'/cpp_quick_guide/10-字符串.md'  },
-          {text:'11.函数',link:'/cpp_quick_guide/11-函数.md'    },
-          {text:'12.递归',link:'/cpp_quick_guide/12-递归.md'    },
-          {text:'13.总结',link:'/cpp_quick_guide/13-总结.md'     }
-        ]
-      },
-      {
-        text:'竞赛路线',
-        collapsible: true,
-        collapsed: false,
-        items:[
-          { text: '路线', link: '/竞赛路线.md' }
-        ]
-      }
-    ]
+    nav: [
+      { text: 'CppBook', link: '/cppbook/' },
+      { text: 'Rbook', link: '/rbook/' },
+      { text: 'Tools', link: '/tools/' }
+    ],
+
+    sidebar: {
+      '/cppbook/' : cppbookSiderBar
+      //{
+        //text:'竞赛路线',
+        //collapsible: true,
+        //collapsed: false,
+        //items:[
+          //{ text: '路线', link: '/竞赛路线.md' }
+        //]
+      //}
+    }
   }
 }
---
+++ cppbook/siderbar.js
@@ -0,0 +1,24 @@
+//cppbook/cppbook sidebar config
+
+module.exports = [
+  {
+    text:'快速入门',
+    collapsible: true,
+    collapsed: false,
+    items:[
+      { text: '1.第一个程序', link: '/cppbook/cpp_quick_guide/1-第一个程序.md' },
+      { text: '2.注释', link: '/cppbook/cpp_quick_guide/2-注释.md' },
+      {text:'3.数据类型',link:'/cppbook/cpp_quick_guide/3-数据类型.md' },
+      {text:'4.输入输出',link:'/cppbook/cpp_quick_guide/4-输入输出.md' },
+      {text:'5.变量',link:'/cppbook/cpp_quick_guide/5-变量.md'     },
+      {text:'6.运算符',link:'/cppbook/cpp_quick_guide/6-运算符.md'   },
+      {text:'7.控制结构',link:'/cppbook/cpp_quick_guide/7-控制结构.md' },
+      {text:'8.循环结构',link:'/cppbook/cpp_quick_guide/8-循环结构.md' },
+      {text:'9.数组',link:'/cppbook/cpp_quick_guide/9-数组.md'     },
+      {text:'10.字符串',link:'/cppbook/cpp_quick_guide/10-字符串.md'  },
+      {text:'11.函数',link:'/cppbook/cpp_quick_guide/11-函数.md'    },
+      {text:'12.递归',link:'/cppbook/cpp_quick_guide/12-递归.md'    },
+      {text:'13.总结',link:'/cppbook/cpp_quick_guide/13-总结.md'     }
+    ]
+  }
+]
